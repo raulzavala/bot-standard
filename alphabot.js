ddb144122bf18ba854e97264980d87387c514749
Raul Zavala <iscraulzavala@gmail.com>
2022-05-27 11:26:02 -0700

    Bot support site included

--- alphabot.js
+++ alphabot.js
@@ -1,17 +1,24 @@
 require("dotenv").config();
+
 const { Client, Intents } = require("discord.js");
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
+
 client.login(process.env.DISCORD_TOKEN);
-// Notify successful connection via console
 client.on("ready", function (e) {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-// Wait for message events, check content for match,
-// respond cordially to user via reply.
+
 client.on("message", function (msg) {
-  if (msg.content === "!Help") {
-    msg.reply("Howdy!");
+  let mesg = msg.content.toLowerCase();
+  if (
+    msg.content === "help" ||
+    msg.content === "support" ||
+    msg.content === "ticket" ||
+    msg.content === "i want support"  ||
+    msg.content === "need support"  
+  ) {
+    msg.reply("Hi, there!. \n Thank you for reaching us. Please open a new ticket on https://app.customersupportnetworks.com/ \n Atari Support");
   }
 });
